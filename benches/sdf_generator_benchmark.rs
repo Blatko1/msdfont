86e48a156cbbb086bcd13df47fe62d9301e5a91c
Blatko1 <kings.of.funny.gemplays@gmail.com>
2022-08-28 21:20:07 +0200

    Benchmark for sdf generator

---
+++ benches/sdf_generator_benchmark.rs
@@ -0,0 +1,17 @@
+use std::f32::consts::PI;
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn sdf_benchmark(crit: &mut Criterion) {
+    // Cubic constants
+    let a = black_box(1.0);
+    let b = black_box(100.4);
+    let c = black_box(-100.4);
+    let d = black_box(-0.29);
+    crit.bench_function("default overlap correction", |bencher| {
+        bencher.iter(|| print!("aa"))
+    });
+}
+
+criterion_group!(benches, sdf_benchmark);
+criterion_main!(benches);
