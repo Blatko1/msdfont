51145322f555b7e848e8c7c66dc87061836cb959
Blatko1 <kings.of.funny.gemplays@gmail.com>
2022-09-02 00:36:28 +0200

    small tests and pain

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,3 +1,25 @@
+use lyon_path::Path;
+use lyon_path::math::{point};
+use lyon_path::builder::*;
+
 fn main() {
     let data = include_bytes!("fonts/monserat.ttf");
+
+    let mut builder = Path::builder();
+
+    // Build a simple path.
+    let mut builder = Path::builder();
+    builder.begin(point(0.0, 0.0));
+    builder.line_to(point(0.0, 2.0));
+    builder.line_to(point(2.0, 2.0));
+    builder.line_to(point(0.9, 1.0));
+    builder.line_to(point(2.0, 0.0));
+    builder.close();
+
+    // Generate the actual path object.
+    let path = builder.build();
+
+    for event in &path {
+        println!("{:?}", event);
+    }
 }
